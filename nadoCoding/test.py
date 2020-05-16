79a035897f4d1a5333b055dbb9169ad0694eaa34
HyungMoYang <42955111+HyungMoYang@users.noreply.github.com>
2020-05-16 23:05:49 +0900

    indeed job scraping

---
+++ nadoCoding/test.py
@@ -0,0 +1,23 @@
+from random import *
+
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+# print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 정수
+
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+# print(randint(1, 45))  # 1 ~ 45 이하의 임의의 정수
+
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월 " + str(randint(4, 28)) + "일로 선정되었습니다.")
